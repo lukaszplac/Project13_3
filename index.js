9435e34d88d2bc448086502c2e507671485998ba
lukaszplac123 <lukasz.plac@gmail.com>
2017-09-06 23:17:03 +0200

    Project 13.3. First release

---
+++ index.js
@@ -0,0 +1,26 @@
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function() {
+	var input = process.stdin.read();
+	if (input !== null) {
+		var instruction = input.toString().trim();
+		switch (instruction) {
+			case '/exit' : quit(); break;
+			case '/ver'	: showVersion(); break;
+			case '/user' : showLogin(); break;
+			default : process.stderr.write('Wrong instruction!\n');
+		}
+
+		function quit() {
+			process.stdout.write('Quiting app!\n');
+			process.exit();
+		}
+
+		function showVersion() {
+			process.stdout.write(process.version + '\n');
+		}
+
+		function showLogin() {
+			process.stdout.write(process.env.username + '\n');
+		}
+	}
+})
